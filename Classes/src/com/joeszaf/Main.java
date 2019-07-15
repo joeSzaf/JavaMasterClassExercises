3c75d6d243be3998834d75298d346f72dcb6f0e1
Joe Szafarowicz <joeszaf+github@gmail.com>
2019-07-15 14:01:23 -0400

    class introduction work

---
+++ Classes/src/com/joeszaf/Main.java
@@ -0,0 +1,12 @@
+package com.joeszaf;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Car porshe = new Car();
+	    Car holden = new Car();
+
+	    porshe.setModel("Carrera");
+		System.out.println("The model is " + porshe.getModel());
+    }
+}
