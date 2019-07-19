2326181b8df689691fbfa9d991dfc28f80c86ac0
Joe Szafarowicz <joeszaf+github@gmail.com>
2019-07-19 13:39:44 -0400

    complex number challenge

---
+++ ComplexOperators/src/com/joeszaf/ComplexNumber.java
@@ -0,0 +1,39 @@
+package com.joeszaf;
+
+public class ComplexNumber {
+    private double real;
+    private double imaginary;
+
+    public ComplexNumber(double real, double imaginary) {
+        this.real = real;
+        this.imaginary = imaginary;
+    }
+
+    public double getReal() {
+        return this.real;
+    }
+
+    public double getImaginary() {
+        return this.imaginary;
+    }
+
+    public void add(double real, double imaginary) {
+        this.real += real;
+        this.imaginary += imaginary;
+    }
+
+    public void add(ComplexNumber newComplex) {
+        this.real += newComplex.getReal();
+        this.imaginary += newComplex.getImaginary();
+    }
+
+    public void subtract(double real, double imaginary) {
+        this.real -= real;
+        this.imaginary -= imaginary;
+    }
+
+    public void subtract(ComplexNumber newComplex) {
+        this.real -= newComplex.getReal();
+        this.imaginary -= newComplex.getImaginary();
+    }
+}
